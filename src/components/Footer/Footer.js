e3b1990172d8054994dd5d2daf63c63d01b2320f
Hyunji Kim <creative.hkim@gmail.com>
2021-04-06 20:14:50 -0400

    Added components for the App component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    </div>
+  )
+}
+
+export default Footer
